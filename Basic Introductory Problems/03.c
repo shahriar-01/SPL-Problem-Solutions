397e936694b9e35dbc95d633119b5f04d62a0de4
Shahriar Hossain <cs.shahriarhossain@gmail.com>
2026-07-06 11:16:42 +0600

    Add files via upload

---
+++ Basic Introductory Problems/03.c
@@ -0,0 +1,8 @@
+//Program that will print the following segment:
+
+#include<stdio.h>
+int main ()
+{
+  printf("The question is- \"How to write a \\comment/ in C programming language?\"\n");
+  return 0;
+}
