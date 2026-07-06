cf29675b6225c8ea30c4f1cd97ba8f78cfd9e9fe
Shahriar Hossain <cs.shahriarhossain@gmail.com>
2026-07-06 11:09:50 +0600

    Add files via upload

---
+++ Basic Introductory Problems/02.c
@@ -0,0 +1,8 @@
+//Program that will use newline/tab and print the following segment. 
+
+#include<stdio.h>
+int main ()
+{
+  printf("Hello World.\nThis is my first program.\t C is fun.");
+  return 0;
+}
