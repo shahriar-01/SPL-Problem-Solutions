2d2dd33c58498714d85fe02f4c04446a04c3d2ce
Shahriar Hossain <cs.shahriarhossain@gmail.com>
2026-07-09 13:25:57 +0600

    Add files via upload

---
+++ Basic Introductory Problems/05.c
@@ -0,0 +1,16 @@
+ /*Program that will do the followings: 
+a) Declare a variable uninitialized 
+b) Declare and  initialize a variable in one statement 
+c) Declare and initialize multiple variables with different values in one statement 
+d) Declare and initialize multiple variables with the same value in one statement */
+
+#include<stdio.h>
+int main ()
+{
+  int k;
+  int c = 10;
+  int i = 12, j = 13;
+  int x,y,z = 2000;
+
+  return 0;
+}
