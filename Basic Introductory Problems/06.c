08ed5de6e35d5f0bc34917746f04d3a7c8e2640c
Shahriar Hossain <cs.shahriarhossain@gmail.com>
2026-07-09 13:26:42 +0600

    Add files via upload

---
+++ Basic Introductory Problems/06.c
@@ -0,0 +1,12 @@
+//Program that will take your age in year(s) as input and print it.//
+
+#include<stdio.h>
+int main()
+{
+  int age;
+  printf("Enter your age: ");
+  scanf("%d", &age);
+
+  printf("My age is: %d", age);
+  return 0;
+}
