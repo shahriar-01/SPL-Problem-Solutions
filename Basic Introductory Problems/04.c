bee8f34b43a85d2b346ec8859b305b8988961e8e
Shahriar Hossain <cs.shahriarhossain@gmail.com>
2026-07-09 12:16:16 +0600

    Add files via upload

---
+++ Basic Introductory Problems/04.c
@@ -0,0 +1,24 @@
+//Program that will declare an integer, a floating point number, a character. Then it will 
+initialize them with values and print those values. 
+
+#include<stdio.h>
+int main()
+{
+  int a = 5;
+  float b = 3.141593;
+  char c ='a';
+
+ printf("The integer value: %d\n", a);
+ printf("The floating point value: %f\n", b);
+ printf("The character value: %c\n", c);
+
+ int d = 100;
+ float e = 1.618000;
+ char f = 'z';
+
+ printf("\nThe integer value: %d\n", d);
+ printf("The floating point value: %f\n", e);
+ printf("The character value: %c\n", f);
+
+ return 0;
+}
