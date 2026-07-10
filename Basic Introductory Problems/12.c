db53d71587cc0ccafc95e5bc3d800b7a0df8e7be
Shahriar Hossain <cs.shahriarhossain@gmail.com>
2026-07-10 19:21:17 +0600

    Add files via upload

---
+++ Basic Introductory Problems/12.c
@@ -0,0 +1,12 @@
+//Program that will define a constant using “CONST” and print the value.//
+
+#include <stdio.h>
+int main()
+{
+    const float pi = 3.14;
+    const float golden_ratio = 1.62;
+
+    printf("The value of pi: %.2f\n",pi);
+    printf("The value of golden ratio: %.2f", golden_ratio);
+    return 0;
+}
