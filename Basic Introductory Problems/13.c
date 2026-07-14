28608cf088433f412bcd4d5bb75c3e1f78c90240
Shahriar Hossain <cs.shahriarhossain@gmail.com>
2026-07-14 10:00:35 +0600

    Add files via upload

---
+++ Basic Introductory Problems/13.c
@@ -0,0 +1,13 @@
+//Program that will define a constant using “DEFINE” and print the value//
+
+#include <stdio.h>
+#define Height 200
+#define PI 3.14
+
+int main ()
+{
+    printf("The value of HEIGHT: %d\n",Height);
+    printf("The value of PI: %.2f", PI);
+    return 0;
+}
+
