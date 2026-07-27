cfca77cfc6f971d7cdd6df677be0ebcaf3676c31
Shahriar Hossain <cs.shahriarhossain@gmail.com>
2026-07-27 18:02:41 +0600

    Add files via upload

---
+++ Operator Related Problems/15.c
@@ -0,0 +1,11 @@
+// Program to find size of int, float, double and char of the system. 
+
+#include <stdio.h>
+int main(){
+  printf("Size of int in byte(s) = %d\n", sizeof(int));
+  printf("Size of float in byte(s) = %d\n", sizeof(float));
+  printf("Size of double in byte(s) = %d\n", sizeof(double));
+  printf("Size of char in byte(s) = %d\n", sizeof(char));
+
+    return 0;
+}
